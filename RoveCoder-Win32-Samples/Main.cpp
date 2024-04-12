6e13456d9e600d27f0202c966a37e3fc7025d777
Callum Anning <CallumAnning@hotmail.co.uk>
2024-04-12 09:40:05 +0100

    Updated create window

--- RoveCoder-Win32-Samples/Main.cpp
+++ RoveCoder-Win32-Samples/Main.cpp
@@ -1,7 +1,62 @@
-#include <iostream>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 
-int main(int argc, char** argv)
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	std::cout << "Hello, Win32\n";
+	switch (uMsg)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+			EndPaint(hwnd, &ps);
+			break;
+		}
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+	const wchar_t className[] = L"MyWindowClass";
+
+	// Register class
+	WNDCLASSEX wc = {};
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.lpszClassName = className;
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WindowProc;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, L"RegisterClassEx failed!", L"Error", MB_OK | MB_ICONERROR);
+		return -1;
+	}
+
+	// Create window
+	HWND hwnd = CreateWindowEx(0, className, L"Window Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, L"CreateWindow failed!", L"Error", MB_OK | MB_ICONERROR);
+		return -1;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	// Main loop
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
 }
